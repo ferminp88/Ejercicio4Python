2321bbfcec028b8c29f19a50d83efbe2f1870c08
Fermin <ferminp88@gmail.com>
2022-11-10 19:42:01 -0300

    Ejercicio 6 python

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,3 +1,5 @@
-año = 2025
 
-print("Es bisiesto" if not año % 4 and (año % 100 or  not año % 400) else "No es bisiesto")
+
+anio: int = int(input("Introduce un año y vamos a ver si es bisiesto... "))
+
+print("Es bisiesto" if not anio % 4 and (anio % 100 or  not anio % 400) else "No es bisiesto")
---
+++ Ejercicio6.py
@@ -0,0 +1,37 @@
+
+class Vehiculo:
+
+    color = "Verde "
+    ruedas = 4
+    puertas = 4
+
+
+class Coche(Vehiculo):
+    velocidad = 150
+    cilindrada = 1800
+
+
+c = Coche()
+
+print("El color del coche es : ", Coche.color)
+print("La cantidad de ruedas es : ",Coche.ruedas)
+print("La cantidad de puertas es: ",Coche.puertas)
+print("La velocidad del coche es: ",Coche.velocidad)
+print("Las cilindradas del coche son: ",Coche.cilindrada)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
